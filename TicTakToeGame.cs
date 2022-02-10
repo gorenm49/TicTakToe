e9ee8fbd04ef06a969be2265671aeaac2105d9f2
Nitya <gorenityanand96@gmail.com>
2022-02-10 10:14:44 +0530

    [Nityanand]:Creating class and Welcomming msg

---
+++ TicTakToeGame.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace TicTakToe
+{
+    public class TicTakToeGame
+    {
+        public void GameFun()
+        {
+            char[] arr = {'1','2','3','4','5','6','7','8','9'};
+            int player_1 =0, player_2=0;
+            
+        }
+    }
+}
